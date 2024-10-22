e13ca6dd1d864ef89859f519921abdebe82cd25a
Verney <verney.sotodezma@gmail.com>
2024-10-22 12:40:18 -0400

    Contact's 'Form' first settings

--- contacto/forms.py
+++ contacto/forms.py
@@ -7,4 +7,4 @@ class FormularioContacto(forms.Form):
 
     email=forms.CharField(label="E-mail", required=True)
     
-    contenido=forms.CharField(label="Contenido")
+    contenido=forms.CharField(label="Contenido",widget=forms.Textarea)
