97f39f77ebdba136525ee31f2f9cded23a87a642
Verney <verney.sotodezma@gmail.com>
2024-11-27 18:51:00 -0400

    Products added to app 'tienda'

--- tienda/admin.py
+++ tienda/admin.py
@@ -1,3 +1,15 @@
+"Models added to 'Administration Panel'"
 from django.contrib import admin
-
+from .models import CategoriaProd, Producto
 # Register your models here.
+
+class CategoriaProdAdmin(admin.ModelAdmin):
+    """A class that define only read fields for 'Categoriaprod' view."""
+    readonly_fields=("created","updated")
+
+class ProductoProdAdmin(admin.ModelAdmin):
+    """A class that define only read fields for 'Producto' view."""
+    readonly_fields=("created","updated")
+
+admin.site.register(CategoriaProd, CategoriaProdAdmin)
+admin.site.register(Producto, ProductoProdAdmin)
--- tienda/views.py
+++ tienda/views.py
@@ -1,8 +1,9 @@
 """The views for the 'tienda' app."""
 from django.shortcuts import render
-
+from .models import Producto
 # Create your views here.
 
 def tienda(request):
     """A view that render 'tienda' web page."""
-    return render(request, "tienda/tienda.html")
+    productos=Producto.objects.all()
+    return render(request, "tienda/tienda.html", {"productos":productos})
