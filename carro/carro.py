591e83a7d644e8ec08ab9f933921b67e1c8e8329
Verney <verney.sotodezma@gmail.com>
2024-11-28 22:51:40 -0400

    Starting out ‘carro’ app (shopping cart)

---
+++ carro/carro.py
@@ -0,0 +1,23 @@
+"""Behavior of the shopping cart"""
+class Carro():
+    """Defining like dictionary to shopping cart."""
+    def __init__(self, request):
+        self.request=request
+        self.session=request.session
+        carro=self.session.get("carro")
+        if not carro:
+            carro=self.session["carro"]={}
+        else:
+            self.carro=carro
+    
+    def agregar(self, producto):
+        """Add products to shopping cart."""
+        if(str(producto.id) not in self.carro.keys()):
+            self.carro[producto.id]={
+                "producto_id":producto.id,
+                "nombre":producto.nombre,
+                "precio":str(producto.precio),
+                "cantidad":1,
+                "imagen":producto.imagen.url,
+                }
+            
